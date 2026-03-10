8d3c1294c49f818b22551cf293641e2393e52d04
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-03-10 09:04:01 -0300

    subindo projeto spring

---
+++ src/main/java/com/pi/apigenatvdcomplementares/ApigenatvdcomplementaresApplication.java
@@ -0,0 +1,13 @@
+package com.pi.apigenatvdcomplementares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigenatvdcomplementaresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigenatvdcomplementaresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pi/apigenatvdcomplementares/ApigenatvdcomplementaresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi.apigenatvdcomplementares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigenatvdcomplementaresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
