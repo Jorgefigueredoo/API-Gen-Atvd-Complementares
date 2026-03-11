3bbce754a8cf4245a31023a97c1a429838e882a8
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-03-11 08:38:08 -0300

    falta terminar, apenas fiz o início

--- src/main/java/com/pi/apigenatvdcomplementares/models/Curso.java
+++ src/main/java/com/pi/apigenatvdcomplementares/models/Curso.java
@@ -1,5 +1,15 @@
 package com.pi.apigenatvdcomplementares.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cursos")
 public class Curso {
     
+    @Id
+    @Column(name = "curso_id", length = 50)
+    private String cursoId;
 }
