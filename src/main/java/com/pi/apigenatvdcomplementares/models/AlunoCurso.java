b3724734ce15f1ed4fb7f2dd770755eb4344e855
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-03-11 19:03:51 -0300

    Tabela de aluno curso criada

---
+++ src/main/java/com/pi/apigenatvdcomplementares/models/AlunoCurso.java
@@ -0,0 +1,28 @@
+package com.pi.apigenatvdcomplementares.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "aluno_curso")
+public class AlunoCurso {
+    
+    @Id
+    @Column(name = "id", length = 36)
+    private String id;
+
+    // Relacionamento com Aluno
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "aluno_id", nullable = false)
+    private Aluno aluno;
+
+    // Relacionamento com Curso
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "curso_id", nullable = false)
+    private Curso curso;
+}
