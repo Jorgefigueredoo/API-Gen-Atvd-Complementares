9678491dc4feb23aea30e58ef85d81d64bbc1080
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-03-11 19:04:38 -0300

    @entity adicionado, e nome das variáveis de curso e coordenador ajustadas

--- src/main/java/com/pi/apigenatvdcomplementares/models/CoordenadorCurso.java
+++ src/main/java/com/pi/apigenatvdcomplementares/models/CoordenadorCurso.java
@@ -1,5 +1,8 @@
 package com.pi.apigenatvdcomplementares.models;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -12,18 +15,23 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 @Table(name = "tb_coordenadores_curso")
 public class CoordenadorCurso extends Auditable {
 
+  @Id
+  @Column(name = "id", length = 50)
+  private String id;
+
   @ManyToOne(fetch = FetchType.LAZY) 
   @JoinColumn(name = "coordenador_id", nullable = false)
-  private Usuario coordenadorId;
+  private Usuario coordenador;
 
   @ManyToOne(fetch = FetchType.LAZY) // 
   @JoinColumn(name = "curso_id", nullable = false)
-  private Curso cursoId;
+  private Curso curso;
 
-
+  
 
 
 }
