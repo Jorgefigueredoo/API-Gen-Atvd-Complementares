6cff804a1767980d45032607b3d41bb061067219
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-03-12 09:05:49 -0300

    Usuário repository feito

---
+++ src/main/java/com/pi/apigenatvdcomplementares/repository/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package com.pi.apigenatvdcomplementares.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pi.apigenatvdcomplementares.models.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, String> {
+
+    Optional<Usuario> findByEmail(String email); // Método para encontrar um usuário pelo email
+
+    boolean existsByEmail(String email); // Método para verificar se um usuário com o email fornecido já existe
+}
