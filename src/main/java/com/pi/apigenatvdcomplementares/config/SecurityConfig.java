bcc7bd61171220819479825604e9d7a93b4e13b5
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-03-16 18:20:33 -0300

    permissão do método post para a criação de novos usuários

--- src/main/java/com/pi/apigenatvdcomplementares/config/SecurityConfig.java
+++ src/main/java/com/pi/apigenatvdcomplementares/config/SecurityConfig.java
@@ -2,29 +2,30 @@ package com.pi.apigenatvdcomplementares.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    return http
-        .csrf(csrf -> csrf.disable())
-        .authorizeHttpRequests(auth -> auth
-            // 🔓 LIBERANDO O SWAGGER E AUTH
-            .requestMatchers(
-                "/api/auth/**",
-                "/v3/api-docs/**",
-                "/swagger-ui/**",
-                "/swagger-ui.html"
-            ).permitAll() 
-            
-            // 🔒 TODO O RESTO EXIGE LOGIN
-            .anyRequest().authenticated())
-        .build();
-  }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/auth/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/error"
+                        ).permitAll()
+                        .requestMatchers(HttpMethod.POST, "/usuarios", "/usuarios/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
 }
