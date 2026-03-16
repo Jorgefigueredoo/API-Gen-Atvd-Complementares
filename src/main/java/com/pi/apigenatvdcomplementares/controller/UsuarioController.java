e59c02de400e094c28cc3330c715d95883019838
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-03-16 18:20:50 -0300

    Usuário controller feito

--- src/main/java/com/pi/apigenatvdcomplementares/controller/UsuarioController.java
+++ src/main/java/com/pi/apigenatvdcomplementares/controller/UsuarioController.java
@@ -1,5 +1,65 @@
 package com.pi.apigenatvdcomplementares.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import com.pi.apigenatvdcomplementares.dto.UsuarioCreateDTO;
+import com.pi.apigenatvdcomplementares.dto.UsuarioDTO;
+import com.pi.apigenatvdcomplementares.models.Usuario;
+import com.pi.apigenatvdcomplementares.service.UsuarioService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/usuarios")
+@Validated
 public class UsuarioController {
-    
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<UsuarioDTO> criarUsuario(@RequestBody @Valid UsuarioCreateDTO dto) {
+        Usuario usuario = new Usuario();
+        usuario.setNome(dto.getNome());
+        usuario.setEmail(dto.getEmail());
+        usuario.setSenha(dto.getSenha());
+        usuario.setPerfil(dto.getPerfil());
+
+        Usuario novoUsuario = usuarioService.salvarUsuario(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new UsuarioDTO(novoUsuario));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioDTO>> listarUsuarios() {
+        List<UsuarioDTO> usuarios = usuarioService.listarUsuarios()
+                .stream()
+                .map(UsuarioDTO::new)
+                .toList();
+
+        return ResponseEntity.ok(usuarios);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioDTO> buscarPorId(@PathVariable Long id) {
+        Usuario usuario = usuarioService.buscarPorId(id);
+        return ResponseEntity.ok(new UsuarioDTO(usuario));
+    }
+
+    @GetMapping("/email/{email}")
+    public ResponseEntity<UsuarioDTO> buscarPorEmail(@PathVariable String email) {
+        Usuario usuario = usuarioService.buscarPorEmail(email);
+        return ResponseEntity.ok(new UsuarioDTO(usuario));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarUsuario(@PathVariable Long id) {
+        usuarioService.deletarUsuario(id);
+        return ResponseEntity.noContent().build();
+    }
 }
