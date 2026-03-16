69d80b6b87fa23b39a94b10699778cbea08b6494
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-03-16 09:11:39 -0300

    ajuste na busca do nome do atributo no model

--- src/main/java/com/pi/apigenatvdcomplementares/repository/CoordenadorRepository.java
+++ src/main/java/com/pi/apigenatvdcomplementares/repository/CoordenadorRepository.java
@@ -1,5 +1,6 @@
 package com.pi.apigenatvdcomplementares.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,10 +11,7 @@ import com.pi.apigenatvdcomplementares.models.CoordenadorCurso;
 @Repository
 public interface CoordenadorRepository extends JpaRepository<CoordenadorCurso, Long> {
 
-    Optional<CoordenadorCurso> findById(Long coordenadorId);
+    Optional<CoordenadorCurso> findByEmail(String email);
 
-    Optional<CoordenadorCurso> findByName(String nome);
-
-    
-    
+    List<CoordenadorCurso> findByCoordenadorNome(String nome);
 }
