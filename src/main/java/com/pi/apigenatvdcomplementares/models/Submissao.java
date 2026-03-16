0d57e1af194e6828f80080fd00a4c826dd4adcfe
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-03-16 18:24:25 -0300

    ajustes de relacionamento

--- src/main/java/com/pi/apigenatvdcomplementares/models/Submissao.java
+++ src/main/java/com/pi/apigenatvdcomplementares/models/Submissao.java
@@ -18,6 +18,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -27,39 +28,40 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity(name = "tb_submissoes")
+@Entity
+@Table(name = "tb_submissoes")
 public class Submissao extends Auditable {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "submissao_id")
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "submissao_id")
+    private Long id;
 
-  @Column(name = "data_submissao", nullable = false)
-  private LocalDateTime dataSubmissao;
+    @Column(name = "data_submissao", nullable = false)
+    private LocalDateTime dataSubmissao;
 
-  @Enumerated(EnumType.STRING)
-  @Column(name = "status_submissao", nullable = false, length = 50)
-  private StatusSubmissao status;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status_submissao", nullable = false, length = 50)
+    private StatusSubmissao status;
 
-  @ElementCollection(fetch = FetchType.EAGER)
-  @CollectionTable(name = "tb_historico_status_submissao", joinColumns = @JoinColumn(name = "submissao_id"))
-  @Enumerated(EnumType.STRING)
-  @Column(name = "historico_status")
-  private Set<StatusSubmissao> historicoStatus;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "tb_historico_status_submissao", joinColumns = @JoinColumn(name = "submissao_id"))
+    @Enumerated(EnumType.STRING)
+    @Column(name = "historico_status")
+    private Set<StatusSubmissao> historicoStatus;
 
-  @OneToMany(mappedBy = "submissao")
-  private Set<Certificado> certificados;
+    @OneToMany(mappedBy = "submissao")
+    private Set<Certificado> certificados;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "aluno_id", nullable = false)
-  private Aluno aluno;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "aluno_id", nullable = false)
+    private Aluno aluno;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "curso_id", nullable = false)
-  private Curso curso;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "curso_id", nullable = false)
+    private Curso curso;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "coordenador_id")
-  private Usuario coordenador;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "coordenador_id")
+    private Usuario coordenador;
 }
