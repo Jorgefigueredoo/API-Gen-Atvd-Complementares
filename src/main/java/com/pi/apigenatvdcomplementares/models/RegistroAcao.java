d02a0f648e5124b3955cca08636141cc5128d422
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-03-16 18:23:32 -0300

    ajustes no id para ele gerar automaticamente ao ser criado um novo usuário

--- src/main/java/com/pi/apigenatvdcomplementares/models/RegistroAcao.java
+++ src/main/java/com/pi/apigenatvdcomplementares/models/RegistroAcao.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -21,8 +23,9 @@ import lombok.Setter;
 public class RegistroAcao extends Auditable {
 
     @Id
-    @Column(name = "id", length = 50)
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "usuario_id", nullable = false)
