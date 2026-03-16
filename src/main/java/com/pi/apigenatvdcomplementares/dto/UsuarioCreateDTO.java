4f83b04899c68ad12d6605dcc5883f1fdf678aa9
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-03-16 18:21:08 -0300

    Usuário Create DTO feito

---
+++ src/main/java/com/pi/apigenatvdcomplementares/dto/UsuarioCreateDTO.java
@@ -0,0 +1,29 @@
+package com.pi.apigenatvdcomplementares.dto;
+
+import com.pi.apigenatvdcomplementares.enums.PerfilUsuario;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UsuarioCreateDTO {
+
+    @NotBlank(message = "Nome é obrigatório")
+    private String nome;
+
+    @NotBlank(message = "Email é obrigatório")
+    @Email(message = "Email inválido")
+    private String email;
+
+    @NotBlank(message = "Senha é obrigatória")
+    private String senha;
+
+    @NotNull(message = "Perfil é obrigatório")
+    private PerfilUsuario perfil;
+}
