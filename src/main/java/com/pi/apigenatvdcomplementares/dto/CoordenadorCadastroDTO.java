eb254b260dd92549256861a6e7fbad0bb9f814b5
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-03-17 15:20:15 -0300

    campo senha adicionado

--- src/main/java/com/pi/apigenatvdcomplementares/dto/CoordenadorCadastroDTO.java
+++ src/main/java/com/pi/apigenatvdcomplementares/dto/CoordenadorCadastroDTO.java
@@ -23,31 +23,10 @@ public class CoordenadorCadastroDTO {
     @NotBlank(message = "O email é obrigatório")
     private String email;
 
+    @NotBlank(message = "A senha é obrigatória")
+    private String senha;
+
     @NotEmpty(message = "Selecione pelo menos um curso")
     private List<Long> cursosIds;
 
-    // Getters e Setters
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public List<Long> getCursosIds() {
-        return cursosIds;
-    }
-
-    public void setCursosIds(List<Long> cursosIds) {
-        this.cursosIds = cursosIds;
-    }
 }
