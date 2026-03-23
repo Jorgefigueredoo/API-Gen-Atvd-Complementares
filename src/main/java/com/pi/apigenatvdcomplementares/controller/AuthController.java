c1400d71adc6d36ebe719dd7f8574951ded735eb
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-03-23 17:36:44 -0300

    remoção da rota register

--- src/main/java/com/pi/apigenatvdcomplementares/controller/AuthController.java
+++ src/main/java/com/pi/apigenatvdcomplementares/controller/AuthController.java
@@ -2,14 +2,8 @@ package com.pi.apigenatvdcomplementares.controller;
 
 import com.pi.apigenatvdcomplementares.dto.LoginRequestDTO;
 import com.pi.apigenatvdcomplementares.dto.LoginResponseDTO;
-import com.pi.apigenatvdcomplementares.dto.RegisterRequestDTO;
-import com.pi.apigenatvdcomplementares.dto.UsuarioDTO;
-import com.pi.apigenatvdcomplementares.enums.PerfilUsuario;
-import com.pi.apigenatvdcomplementares.models.Usuario;
 import com.pi.apigenatvdcomplementares.security.JwtService;
-import com.pi.apigenatvdcomplementares.service.UsuarioService;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -23,16 +17,13 @@ public class AuthController {
 
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
-    private final UsuarioService usuarioService;
 
     public AuthController(
             AuthenticationManager authenticationManager,
-            JwtService jwtService,
-            UsuarioService usuarioService
+            JwtService jwtService
     ) {
         this.authenticationManager = authenticationManager;
         this.jwtService = jwtService;
-        this.usuarioService = usuarioService;
     }
 
     @PostMapping("/login")
@@ -48,20 +39,5 @@ public class AuthController {
         String token = jwtService.generateToken(userDetails);
 
         return ResponseEntity.ok(new LoginResponseDTO(token, "Bearer"));
-    }
-
-    @PostMapping("/register")
-    public ResponseEntity<UsuarioDTO> register(@RequestBody @Valid RegisterRequestDTO request) {
-        Usuario usuario = new Usuario();
-        usuario.setNome(request.getNome());
-        usuario.setEmail(request.getEmail());
-        usuario.setSenha(request.getSenha());
-        usuario.setPerfil(
-                request.getPerfil() != null ? request.getPerfil() : PerfilUsuario.ALUNO
-        );
-
-        Usuario novoUsuario = usuarioService.salvarUsuario(usuario);
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(new UsuarioDTO(novoUsuario));
     }
 }
