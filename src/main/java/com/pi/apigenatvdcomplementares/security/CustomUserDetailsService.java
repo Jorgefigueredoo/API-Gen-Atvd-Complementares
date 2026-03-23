183b0545efa7ab5c3e6829bceedbdf0912932630
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-03-23 17:33:41 -0300

    serviço para buscar usuário e entregar ao spring security o email e a senha

---
+++ src/main/java/com/pi/apigenatvdcomplementares/security/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.pi.apigenatvdcomplementares.security;
+
+import com.pi.apigenatvdcomplementares.models.Usuario;
+import com.pi.apigenatvdcomplementares.repository.UsuarioRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public CustomUserDetailsService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com o email: " + email));
+
+        return User.builder()
+                .username(usuario.getEmail())
+                .password(usuario.getSenha())
+                .authorities(List.of(new SimpleGrantedAuthority("ROLE_" + usuario.getPerfil().name())))
+                .build();
+    }
+}
