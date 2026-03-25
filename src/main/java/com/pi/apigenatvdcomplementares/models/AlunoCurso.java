eb57d628f0bdceee84fd0ad0eb375227fde40af9
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-03-25 16:46:12 -0300

    ajustes para id gerar automaticamente

--- src/main/java/com/pi/apigenatvdcomplementares/models/AlunoCurso.java
+++ src/main/java/com/pi/apigenatvdcomplementares/models/AlunoCurso.java
@@ -1,26 +1,40 @@
 package com.pi.apigenatvdcomplementares.models;
 
+import java.util.UUID;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity(name = "tb_aluno_curso")
+@Getter
+@Setter
+@NoArgsConstructor
 public class AlunoCurso {
     
     @Id
     @Column(name = "id", length = 36)
     private String id;
 
-    // Relacionamento com Aluno
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "aluno_id", nullable = false)
     private Aluno aluno;
 
-    // Relacionamento com Curso
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "curso_id", nullable = false)
     private Curso curso;
+
+    @PrePersist
+    public void gerarId() {
+        if (this.id == null) {
+            this.id = UUID.randomUUID().toString();
+        }
+    }
 }
