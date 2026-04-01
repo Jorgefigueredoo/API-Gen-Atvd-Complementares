1ad4068b5f76c8c6674f4bcefbc7bf57d277a7cb
Vitor Santos <vitorvitorsantos2015@hotmail.com>
2026-04-01 08:35:35 -0300

    fix: remoção dos comentarios

--- src/main/java/com/pi/apigenatvdcomplementares/models/AlunoCurso.java
+++ src/main/java/com/pi/apigenatvdcomplementares/models/AlunoCurso.java
@@ -3,8 +3,8 @@ package com.pi.apigenatvdcomplementares.models;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue; // <-- Verifique se tem este import
-import jakarta.persistence.GenerationType; // <-- Verifique se tem este import
+import jakarta.persistence.GeneratedValue; 
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
